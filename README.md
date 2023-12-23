248129e197baebe1438b9570301a1a5def6191e3
hassan4mz <57098971+hassan4mz@users.noreply.github.com>
2023-12-23 10:10:56 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+Step 1 : Run This Command in Openwrt SSH
+```
+rm -f ou.sh && wget https://raw.githubusercontent.com/hassan4mz/openwrt-auto-update/main/ou.sh && chmod 777 ou.sh && sh ou.sh
+```
---
+++ ou.sh
@@ -0,0 +1,17 @@
+rm /home/user/openwrt/target/linux/ramips/dts/mt7621_mercury_KM06-704H-v1.dts
+rm /home/user/openwrt/target/linux/ramips/dts/mt7621_mercury_KM06-704H-v2.dts
+rm /home/user/openwrt/target/linux/ramips/dts/mt7621_mercury_KM06-708H-v1.dts
+rm /home/user/openwrt/target/linux/ramips/dts/mt7621_mercury_KM06-708H-v2.dts
+cd /home/user/openwrt/target/linux/ramips/dts/
+
+wget https://raw.githubusercontent.com/hopenwrt-auto-update/edit/main/mt7621_mercury_KM06-704H-v1.dts
+wget https://raw.githubusercontent.com/hopenwrt-auto-update/edit/main/mt7621_mercury_KM06-708H-v1.dts
+wget https://raw.githubusercontent.com/hopenwrt-auto-update/edit/main/mt7621_mercury_KM06-704H-v2.dts
+wget https://raw.githubusercontent.com/hopenwrt-auto-update/edit/main/mt7621_mercury_KM06-708H-v2.dts
+
+
+
+rm /home/hassan/openwrt/target/linux/ramips/image/mt7621.mk
+cd /home/hassan/openwrt/target/linux/ramips/image
+wget https://raw.githubusercontent.com/hopenwrt-auto-update/edit/main/mt7621.mk
+
