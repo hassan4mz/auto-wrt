ad050eb602f608f504751ebce796645d2ebaf1d8
hassan4mz <57098971+hassan4mz@users.noreply.github.com>
2023-12-23 11:13:31 +0200

    Update ou.sh

--- ou.sh
+++ ou.sh
@@ -1,13 +1,13 @@
 rm /home/user/openwrt/target/linux/ramips/dts/mt7621_mercury_KM06-704H-v1.dts
 rm /home/user/openwrt/target/linux/ramips/dts/mt7621_mercury_KM06-704H-v2.dts
-rm /home/user/openwrt/target/linux/ramips/dts/mt7621_mercury_KM06-708H-v1.dts
-rm /home/user/openwrt/target/linux/ramips/dts/mt7621_mercury_KM06-708H-v2.dts
+rm /home/user/openwrt/target/linux/ramips/dts/mt7621_mercury_KM08-708H-v1.dts
+rm /home/user/openwrt/target/linux/ramips/dts/mt7621_mercury_KM08-708H-v2.dts
 cd /home/user/openwrt/target/linux/ramips/dts/
 
 wget https://raw.githubusercontent.com/hassan4mz/auto-wrt/main/mt7621_mercury_KM06-704H-v1.dts
-wget https://raw.githubusercontent.com/hassan4mz/auto-wrt/main/mt7621_mercury_KM06-708H-v1.dts
+wget https://raw.githubusercontent.com/hassan4mz/auto-wrt/main/mt7621_mercury_KM08-708H-v1.dts
 wget https://raw.githubusercontent.com/hassan4mz/auto-wrt/main/mt7621_mercury_KM06-704H-v2.dts
-wget https://raw.githubusercontent.com/hassan4mz/auto-wrt/main/mt7621_mercury_KM06-708H-v2.dts
+wget https://raw.githubusercontent.com/hassan4mz/auto-wrt/main/mt7621_mercury_KM08-708H-v2.dts
 
 
 
