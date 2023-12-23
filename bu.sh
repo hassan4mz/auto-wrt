4e43de882df9fd615ddb01def4959bec9242c75f
hassan4mz <57098971+hassan4mz@users.noreply.github.com>
2023-12-23 14:30:52 +0200

    Update bu.sh

--- bu.sh
+++ bu.sh
@@ -1,9 +1,6 @@
-package/base-files/files/bin/config_generate
-M       package/base-files/files/etc/banner
-M       package/kernel/mac80211/files/lib/wifi/mac80211.sh
-M       package/network/services/dnsmasq/files/dhcp.conf
-M       target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
-
+rm /home/user/openwrt/package/base-files/files/bin/config_generate
+cd /home/user/openwrt/package/base-files/files/bin/
+wget https://raw.githubusercontent.com/hassan4mz/auto-wrt/main/config_generate
 
 
 rm /home/user/openwrt/package/base-files/files/etc/banner
