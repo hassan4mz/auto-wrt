df364e9af4e86a190c0500d8f471a98992698206
hassan4mz <57098971+hassan4mz@users.noreply.github.com>
2023-12-23 14:15:57 +0200

    Create bu.sh

---
+++ bu.sh
@@ -0,0 +1,5 @@
+package/base-files/files/bin/config_generate
+M       package/base-files/files/etc/banner
+M       package/kernel/mac80211/files/lib/wifi/mac80211.sh
+M       package/network/services/dnsmasq/files/dhcp.conf
+M       target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
