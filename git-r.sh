914f0b7dd9167de55c815418aa5de62cf368b102
hassan4mz <57098971+hassan4mz@users.noreply.github.com>
2023-12-26 12:52:57 +0200

    Update git-r.sh

--- git-r.sh
+++ git-r.sh
@@ -1,5 +1,6 @@
 
 
+cd /home/user/openwrt
 git restore /home/user/openwrt/package/base-files/files/bin/config_generate
 git restore /home/user/openwrt/package/base-files/files/etc/banner /
 git restore /home/user/openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh 
