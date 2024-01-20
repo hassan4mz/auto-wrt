1e5b118c4d678228dbcbb8fdc729b6bb55ab4154
hassan4mz <57098971+hassan4mz@users.noreply.github.com>
2024-01-20 10:50:04 +0200

    Update bu.sh

--- bu.sh
+++ bu.sh
@@ -1,20 +1,20 @@
-
+#   ________________________   Change Ip-Addresse ____________________
 sed -i "s/192.168.1/192.168.10/" /home/user/openwrt/package/base-files/files/bin/config_generate
 
 rm /home/user/openwrt/package/base-files/files/etc/banner
 cd /home/user/openwrt/package/base-files/files/etc/
 wget https://raw.githubusercontent.com/hassan4mz/auto-wrt/main/banner
 
+#   ________________________   Change SSID ____________________
 
-#rm /home/user/openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
-#cd /home/user/openwrt/package/kernel/mac80211/files/lib/wifi/
-#wget https://raw.githubusercontent.com/hassan4mz/auto-wrt/main/mac80211.sh
 sed -i "s/.ssid=OpenWrt/.ssid=JM-WIFI/" /home/user/openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 
-rm /home/user/openwrt/package/network/services/dnsmasq/files/dhcp.conf
-cd /home/user/openwrt/package/network/services/dnsmasq/files/
-wget https://raw.githubusercontent.com/hassan4mz/auto-wrt/main/dhcp.conf
+#   ________________________   DHCP Enable /Disable  ____________________
+
+#sed -i "s/.ssid=OpenWrt/.ssid=JM-WIFI/" /home/user/openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i '$a	option interface	wan/	option interface	wan \
+#   option ignore	1' /home/user/openwrt/package/network/services/dnsmasq/files/dhcp.conf
 
 rm /home/user/openwrt/target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
 cd  /home/user/openwrt/target/linux/ramips/mt7621/base-files/lib/upgrade/
